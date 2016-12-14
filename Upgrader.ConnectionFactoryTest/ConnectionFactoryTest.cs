2f7e00baaf1a277dd217618c682d83c41f8c5eb6
Oskar Sjöberg <oskar@oskarsjoberg.se>
2016-12-14 18:16:12 +0100

    Added test for ConnectionFactory.

---
+++ Upgrader.ConnectionFactoryTest/ConnectionFactoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Upgrader.MySql;
+
+namespace Upgrader.ConnectionFactoryTest
+{
+    [TestClass]
+    public class ConnectionFactoryTest
+    {
+        [TestMethod]
+        public void CanLoad()
+        {
+            new MySqlDatabase("MySql");
+        }
+    }
+}
